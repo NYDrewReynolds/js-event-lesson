ebdc1441ec2cfbb12cfb411d94a80fb322786335
Drew Reynolds <NYDrewReynolds@gmail.com>
2015-09-02 11:31:27 -0600

    Complete Keycode lesson

--- 04-keycodes/script.js
+++ 04-keycodes/script.js
@@ -6,14 +6,30 @@ var block = {
   height: 20,
   x: 190,
   y: 140,
-}
+};
 
 document.addEventListener('keydown', function (event) {
-  // Your code here…
+  if (event.which === 39){ //right
+      block.x ++;
+  }else if(event.which === 37){ //left
+      block.x --;
+  }else if(event.which === 38){ //up
+      block.y --;
+  }else if(event.which === 40){ //down
+      block.y ++;
+  }
 });
 
 requestAnimationFrame(function gameLoop() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.fillRect(block.x, block.y, block.width, block.height);
   requestAnimationFrame(gameLoop);
+});
+
+var tf = document.querySelector('#text-field');
+
+tf.addEventListener('keyup', function(event) {
+  if(event.keyCode === 65) {
+    console.log("A was pressed!");
+  }
 });
